a20fe67ca20bede901714e835d05c3738fb372f8
23072rh <23072rh@hvhs.school.nz>
2025-06-06 03:08:56 +0000

    Jun 6, 2025, 3:08 AM

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function start() {
     //Asking the user how many seats they want in their car
     userSeats = prompt("So " + userName + "...\nHow many seats would you like inside your car?");
     while (userSeats < 1 || userSeats > 7) {
-        userSeats = prompt("Sorry " + userName + " we don't have any cars in stock with " + userSeats + " seats.\nPlease pick a number of seats between...\n 1 - 7");
+        userSeats = prompt("Sorry " + userName + " we don't have any cars in stock with " + userSeats + " seats.\nPlease pick a number of seats between...\n 1 - 8");
     }
     alert(userSeats + " seats is a good choice " + userName + "!");
     //If the user would like one seat in their car
@@ -40,30 +40,30 @@ function start() {
     }
     //If the user would like two seats in their car
     if (userSeats == 2) {
-        prompt("You want one seat in your car, we have a Maxda MX5 in stock just for you.");
+        prompt("You want two seats in your car, we have a Maxda MX5 in stock just for you.");
     }
     //If the user would like three seats in their car
     if (userSeats == 3) {
-        prompt("You want one seat in your car, we have a Toyota Yaris in stock just for you.");
+        prompt("You want three seats in your car, we have a Toyota Yaris in stock just for you.");
     }
     //If the user would like four seats in their car
     if (userSeats == 4) {
-        prompt("You want one seat in your car, we have a Mini Cooper in stock just for you.");
+        prompt("You want four seats in your car, we have a Mini Cooper in stock just for you.");
     }
     //If the user would like five seats in their car
     if (userSeats == 5) {
-        prompt("You want one seat in your car, we have a KIA Sportage in stock just for you.");
+        prompt("You want five seats in your car, we have a KIA Sportage in stock just for you.");
     }
     //If the user would like six seats in their car
     if (userSeats == 6) {
-        prompt("You want one seat in your car, we have a ____ in stock just for you.");
+        prompt("You want six seats in your car, we have a Mitsubishi Triton in stock just for you.");
     }
     //If the user would like seven seats in their car
     if (userSeats == 7) {
-        prompt("You want one seat in your car, we have a ____ in stock just for you.");
+        prompt("You want seven seats in your car, we have a Mitsubishi Outlander in stock just for you.");
     }
     //If the user would like eight seats in their car
     if (userSeats == 8) {
-        prompt("You want one seat in your car, we have a KIA Carnival in stock just for you.");
+        prompt("You want eight seats in your car, we have a KIA Carnival in stock just for you.");
     }
 }
