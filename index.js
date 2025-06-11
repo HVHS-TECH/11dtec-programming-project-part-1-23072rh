fc1f95188257bc59ddfbe422933903ee90f71ddf
23072rh <23072rh@hvhs.school.nz>
2025-06-11 07:39:51 +0000

    Jun 11, 2025, 7:39 AM

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let userChoice;
 //userSeats variable
 let userSeats;
 /**************
-carArray Array
+Cars availble for rent 
 **************/
 //                0         1            2              3              4               5                  6                    7                   8      
 let carArray = ["Walk", "BAC Mono", "Mazda MX5", "Toyota Yaris", "Mini Cooper", "KIA Sportage", "Mitsubishi Triton", "Mitsubishi Outlander", "KIA Carnival"];
